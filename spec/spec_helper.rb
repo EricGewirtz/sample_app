0ea85326c7180019373c8252f4bba1ccaca6ffb4
Eric Gewirtz <eric.gewirtz@avaion.com>
2014-01-11 18:57:35 -0500

    created home - help - about static pages - incorporated dynamical title

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  
+  #added based om instructions from tutorial - esg - 2014-01-11
+  config.include Capybara::DSL
 end
