834a1a606070a21b8c331d70ca46fc0e411e5409
Eric Gewirtz <eric.gewirtz@avaion.com>
2014-02-09 14:42:27 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  #get "users/new"
   #get "static_pages/home"
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
+  resources :users
   # uneceesary to use match '/home',    to: 'static_pages#home',    via: 'get' - rails uses root  'static_pages#home'
   root 'static_pages#home'
   #replace static routes with dynamic routes - automatically builds the named routes - help_path, about_path, contact_path
